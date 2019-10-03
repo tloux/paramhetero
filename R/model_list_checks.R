44cacf91a82a6e1c4ba07e79a665a6ae4155774e
tloux <travis.loux@gmail.com>
2019-10-03 09:55:08 -0500

    Move model_list_checks to own file
    Attempt to fix residual_plots error

--- R/model_list_checks.R
+++ R/model_list_checks.R
@@ -41,28 +41,3 @@ model_list_checks = function(model_list){
   }
 
 }
-
-
-
-
-model_names_checks = function(model_list, model_names){
-
-
-  # check model names same length as model list -----------
-
-  if(!is.null(model_names)){
-    if(length(model_names) != length(model_list)){
-      stop('model_list and model_names not same length.')
-    }
-  }
-
-
-  # check model names are unique --------------------------
-
-  if (!is.null(model_names)){
-    if(length(unique(model_names)) != length(model_names)){
-      stop('model_names not unique.')
-    }
-  }
-
-}
