e079c772e124a2d8857ea4d5ab2049d8ddc6783c
tloux <travis.loux@gmail.com>
2019-10-03 09:56:24 -0500

    See previous

---
+++ R/model_names_checks.R
@@ -0,0 +1,21 @@
+model_names_checks = function(model_list, model_names){
+  
+  
+  # check model names same length as model list -----------
+  
+  if(!is.null(model_names)){
+    if(length(model_names) != length(model_list)){
+      stop('model_list and model_names not same length.')
+    }
+  }
+  
+  
+  # check model names are unique --------------------------
+  
+  if (!is.null(model_names)){
+    if(length(unique(model_names)) != length(model_names)){
+      stop('model_names not unique.')
+    }
+  }
+  
+}
