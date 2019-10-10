7918f14fb00e346b03b7d4fc344f9bf459f2c5aa
tloux <travis.loux@gmail.com>
2019-10-10 14:18:11 -0500

    Another minor error

--- R/get_coefs.R
+++ R/get_coefs.R
@@ -8,7 +8,7 @@ get_coefs.default = function(model){
 
   int_index = grep(pattern='(I|i)ntercept', x=names(coef(model)))
 
-  if(length(int_ind) > 0){
+  if(length(int_index) > 0){
     return(coef(model)[-int_index])
   }else{
     return(coef(model))
@@ -20,7 +20,7 @@ get_coefs.lmerMod = function(model){
 
   int_index = grep(pattern='(I|i)ntercept', x=names(fixef(model)))
 
-  if(length(int_ind > 0)){
+  if(length(int_index > 0)){
     return(fixef(model)[-int_index])
   }else{
     return(fixef(model))
