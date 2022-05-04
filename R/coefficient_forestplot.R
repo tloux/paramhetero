1969ba264bfdbe36316c1f6c716e32b63d8c59ad
Travis Loux <loux@slu.edu>
2022-05-04 11:56:59 -0500

    1.1.7 update forestplot

--- R/coefficient_forestplot.R
+++ R/coefficient_forestplot.R
@@ -66,7 +66,7 @@ coefficient_forestplot = function(model_list, model_names = NULL,
 
   ci_list = lapply(1:length(model_list), function(m){
 
-    if(class(model_list[[m]]) == 'svyolr'){
+    if(class(model_list[[m]])[1] == 'svyolr'){
       n_zeta = length(model_list[[m]]$lev) - 1
       last_coef = length(coef(model_list[[m]])) - n_zeta
       tmp = as.data.frame(confint(model_list[[m]],
